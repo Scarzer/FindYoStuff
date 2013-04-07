7a37176be0005a9218f2a73c0a5375665dfadb31
Irvy <Irving.Derin@gmail.com>
2013-04-07 01:54:34 -0400

    Snapshot commit

---
+++ helpers/mongooseHelper.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+// A Schema is a template for a generalized object
+// Use this to create a blueprint of your object!
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId,
+
+
+
+
